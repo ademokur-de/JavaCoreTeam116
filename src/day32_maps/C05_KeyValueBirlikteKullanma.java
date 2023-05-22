b49b27c10d8133f7ad7ea123b49c444a4dfc84b5
AdemOkurDE <ademokur.tr@gmail.com>
2023-05-22 22:58:42 +0200

    2023Mayis22

--- src/day32_maps/C05_KeyValueBirlikteKullanma.java
+++ src/day32_maps/C05_KeyValueBirlikteKullanma.java
@@ -6,15 +6,10 @@ public class C05_KeyValueBirlikteKullanma {
     public static void main(String[] args) {
         Map<Integer,String> ogrenciMap = MapMethodlar.ogrenciMapOlustur();
 
-
         int basNo=90;
         int bitNo=104;
 
-
-
         MapMethodlar.numaraAraligindakiOgrencileriyazdir(ogrenciMap,basNo,bitNo);
-
-
 
     }
 }
