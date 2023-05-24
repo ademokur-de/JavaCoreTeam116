903b14481ab179d4fcc22adde8008b80beebd5b3
AdemOkurDE <ademokur.tr@gmail.com>
2023-05-24 22:31:58 +0200

    2023Mayis24

---
+++ src/Aramizda/Maps_Sorular_Elif_H.java
@@ -0,0 +1,27 @@
+package Aramizda;
+
+public class Maps_Sorular_Elif_H {
+
+    /*
+        S1: Belirli bir listede tekrarlanan öğelerin sayısı nasıl kontrol edilir?
+            List<Integer> myList = new ArrayList<>();
+            myList.add(12);
+            myList.add(21);
+            myList.add(12);
+            myList.add(13);
+            myList.add(12);
+            myList.add(21);
+            myList.add(35);
+            Map<Integer, Integer> myMap = new HashMap<>();
+     */
+    /*
+        S2: Aşağıda key olarak ürün adlarını ve value olarak ürün sayısını içeren bir map
+            bulunmaktadır. Ürün adlarını alfabetik sırayla yazdırınız.
+            Map<String, Integer> product = new HashMap<>();
+            product.put("Laptop", 82);
+            product.put("TV", 53);
+            product.put("Refrigerator", 12);
+            product.put("Music System", 87);
+            product.put("Mobile Phone", 53);
+     */
+}
